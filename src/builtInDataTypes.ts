b50c89283acd05b933c7c4d726ea555aef84bcd2
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-10-30 15:33:26 +0530

    Built in data types.

---
+++ src/builtInDataTypes.ts
@@ -0,0 +1,6 @@
+// Below expression can also be written without using ': number' and typescript will automatically know next time that it is a number and will restrict assigning the non-number values to the variable.
+let num: number = 34_234;
+// let num = 34_234; /* It is also valid and num will be of type number. */
+
+let str: string = "Raza";
+let isOkay: boolean = true;
